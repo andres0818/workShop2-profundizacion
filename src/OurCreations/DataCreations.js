6cd0b944430afb5819a413aea0fb414a3a6798c3
Jaredzu <jared.zu96@gmail.com>
2023-01-13 14:47:38 -0500

    Seccion Creations

---
+++ src/OurCreations/DataCreations.js
@@ -0,0 +1,54 @@
+import DeepEarth from './img/image-deep-earth.jpg'
+import Night from './img/image-night-arcade.jpg'
+import Soccer from './img/image-soccer-team.jpg'
+import Grid from './img/image-grid.jpg'
+import Above from './img/image-from-above.jpg'
+import Pocket from './img/image-pocket-borealis.jpg'
+import Curiosity from './img/image-curiosity.jpg'
+import Fisheye from './img/image-fisheye.jpg'
+
+
+const data = [
+  {
+    id: 1,
+    imagen: DeepEarth,
+    titulo: 'Deep earth'
+  },
+  {
+    id: 2,
+    imagen: Night,
+    titulo: 'Night arcade'
+  },
+  {
+    id: 3,
+    imagen: Soccer,
+    titulo: 'Soccer Team Vr'
+  },
+  {
+    id: 4,
+    imagen: Grid,
+    titulo: 'The grid'
+  },
+  {
+    id: 5,
+    imagen: Above,
+    titulo: 'From Up above vr'
+  },
+  {
+    id: 6,
+    imagen: Pocket,
+    titulo: 'Pocket Borealis'
+  },
+  {
+    id: 7,
+    imagen: Curiosity,
+    titulo: 'The curiosity'
+  },
+  {
+    id: 8,
+    imagen: Fisheye,
+    titulo: 'Make it fisheye'
+  }
+]
+
+export default data
---
+++ src/OurCreations/OurCreations.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './OurCreations.scss'
+import data from './DataCreations'
+
+const OurCreations = () => {
+  const [cartas, setCartas] = React.useState(data)
+
+  return (
+    <>
+      <div className='seccion-titulo'>
+        <h2 className='titulo'>OUR CREATIONS</h2>
+        <button className='boton'>See All</button>
+      </div>
+
+      <div className='seccion-cartas'>
+        {cartas.map(carta => {
+          return (
+            <div className='carta'>
+              <div className='contenido-carta'>
+                <img src={carta.imagen} alt='creation card' />
+                <h3 className='subtitulo'>{carta.titulo}</h3>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default OurCreations
