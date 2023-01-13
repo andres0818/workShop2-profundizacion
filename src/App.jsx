e4fb6deaff4beab49c2115fee55274def7bb4447
andres0818 <c.andres.perez.h@gmail.com>
2023-01-13 15:11:34 -0500

    node_module sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-
 import Main from './Main/Main'
-
 import Cabecera from './components/Cabecera/Cabecera'
-import Informacion from './components/Informacion/Informacion'
 import Footer from './components/Footer/Footer'
 
-
 import './App.scss'
+
 
 const App = () => {
   return (
     <>
       <Cabecera />
       <Main />
-      <Informacion />
       <Footer />
     </>
   )
