e2c106340066f6e3e090f8fd1c3abed3c816fdee
andres0818 <c.andres.perez.h@gmail.com>
2023-01-13 12:36:27 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import Cabecera from './Cabecera/Cabecera'
-import Informacion from './Informacion/Informacion'
+import Cabecera from './components/Cabecera/Cabecera'
+import Informacion from './components/Informacion/Informacion'
+import Footer from './components/Footer/Footer'
 
 import './App.scss'
 
 const App = () => {
   return (
-   <>
-    {/* <Cabecera/> 
-   <Informacion/> */}
-   </>
+    <>
+      <Cabecera />
+      <Informacion />
+      <Footer />
+    </>
   )
 }
 
