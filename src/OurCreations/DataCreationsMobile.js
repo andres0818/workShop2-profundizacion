1087857347cebe87aa04e512f4b7d4f0b8d4592c
Jaredzu <jared.zu96@gmail.com>
2023-01-13 15:40:10 -0500

    Creations Version Mobile y responsive

---
+++ src/OurCreations/DataCreationsMobile.js
@@ -0,0 +1,54 @@
+import DeepEarth from './imgMobile/image-deep-earth.jpg'
+import Night from './imgMobile/image-night-arcade.jpg'
+import Soccer from './imgMobile/image-soccer-team.jpg'
+import Grid from './imgMobile/image-grid.jpg'
+import Above from './imgMobile/image-from-above.jpg'
+import Pocket from './imgMobile/image-pocket-borealis.jpg'
+import Curiosity from './imgMobile/image-curiosity.jpg'
+import Fisheye from './imgMobile/image-fisheye.jpg'
+
+
+const dataMobile = [
+  {
+    id: 1,
+    imagen: DeepEarth,
+    titulo: 'Deep earth'
+  },
+  {
+    id: 2,
+    imagen: Night,
+    titulo: 'Night arcade'
+  },
+  {
+    id: 3,
+    imagen: Soccer,
+    titulo: 'Soccer Team Vr'
+  },
+  {
+    id: 4,
+    imagen: Grid,
+    titulo: 'The grid'
+  },
+  {
+    id: 5,
+    imagen: Above,
+    titulo: 'From Up above vr'
+  },
+  {
+    id: 6,
+    imagen: Pocket,
+    titulo: 'Pocket Borealis'
+  },
+  {
+    id: 7,
+    imagen: Curiosity,
+    titulo: 'The curiosity'
+  },
+  {
+    id: 8,
+    imagen: Fisheye,
+    titulo: 'Make it fisheye'
+  }
+]
+
+export default dataMobile
--- src/OurCreations/OurCreations.jsx
+++ src/OurCreations/OurCreations.jsx
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './OurCreations.scss'
 import data from './DataCreations'
+import dataMobile from './DataCreationsMobile'
 
 const OurCreations = () => {
-  const [cartas, setCartas] = React.useState(data)
+  const cartas = data
+
+  const cartasMobile = dataMobile
 
   return (
     <>
-      <div className='seccion-titulo'>
-        <h2 className='titulo'>OUR CREATIONS</h2>
-        <button className='boton'>See All</button>
+      <div className='escritorio'>
+        <div className='seccion-titulo'>
+          <h2 className='titulo'>OUR CREATIONS</h2>
+          <button className='boton'>See All</button>
+        </div>
+
+        <div className='seccion-cartas'>
+          {cartas.map(carta => {
+            return (
+              <div className='carta'>
+                <div className='contenido-carta'>
+                  <img
+                    className='imagen'
+                    src={carta.imagen}
+                    alt='creation card'
+                  />
+                  <h3 className='subtitulo'>{carta.titulo}</h3>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
 
-      <div className='seccion-cartas'>
-        {cartas.map(carta => {
-          return (
-            <div className='carta'>
-              <div className='contenido-carta'>
-                <img className='imagen' src={carta.imagen} alt='creation card' />
-                <h3 className='subtitulo'>{carta.titulo}</h3>
+      {/*-------------MOBILE VERSION-------*/}
+      <div className='mobile'>
+        <div className='seccion-titulo'>
+          <h2 className='titulo'>OUR CREATIONS</h2>
+        </div>
+        <div className='seccion-cartas-mobile'>
+          {cartasMobile.map(carta => {
+            return (
+              <div className='carta'>
+                <div className='contenido-carta'>
+                  <img
+                    className='imagen'
+                    src={carta.imagen}
+                    alt='creation card'
+                  />
+                  <h3 className='subtitulo'>{carta.titulo}</h3>
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
+        <button className='boton'>See All</button>
       </div>
     </>
   )
